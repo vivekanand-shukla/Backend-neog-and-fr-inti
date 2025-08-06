1588ea315a4cdc99d7170a91c952bbca0334495e
VIVEK <primevivek14@gmail.com>
2025-08-06 15:31:59 +0530

    added BE3.3_CW

---
+++ BE3.3_CW/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+
+const cars=[ 
+    {id:1 ,make:"toyota" , model:"camry"  ,year:2022},
+    {id:2 ,make:"toyota" , model:"camry"  ,year:2022},
+    {id:3 ,make:"toyota" , model:"camry"  ,year:2022},
+    {id:4 ,make:"toyota" , model:"camry"  ,year:2022},
+    {id:5 ,make:"toyota" , model:"camry"  ,year:2022},
+    {id:6 ,make:"toyota" , model:"camry"  ,year:2022},
+    {id:7 ,make:"toyota" , model:"camry"  ,year:2022},
+]
+
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.send("HEllo expres")
+})
+app.post('/cars' ,(req,res)=>{
+    const newCar = req.body
+    if(!newCar.make || !newCar.model || !newCar.year){
+        res.status(400).json({error:"make model and year are required"})
+    }else{
+       cars.push(newCar) 
+       res.status(201).json({message:"Car added successfuly . " , car:newCar})
+    }
+})
+app.get('/cars',(req,res)=>{
+    res.send(cars)
+} )
+
+app.delete('/cars/:id' ,(req,res)=>{
+    const carId = req.params.id
+    const index = cars.findIndex(car => car.id == carId)
+    if(index === -1){
+   res.status(404).json({error:'car not found '})
+    }else{
+        cars.splice(index ,1)
+        res.status(200).json({massage:"car deleted sucessfuly"})
+    }
+    
+
+})
+const PORT =  3000
+app.listen(PORT ,()=>{
+    console.log(`server running on port ${PORT}`)
+})
